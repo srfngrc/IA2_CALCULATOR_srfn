e5886901fd67404caa0a406e993468c7db9b649f
srfngrc <pascuallopez112@yahoo.es>
2019-06-02 23:24:03 -0400

    june 2nd 23h

--- weigth_convertor_Form.Designer.cs
+++ weigth_convertor_Form.Designer.cs
@@ -28,19 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.TBlbsTOkgs = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // weigth_convertorForm
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(29, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 99);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(274, 67);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(109, 99);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // TBlbsTOkgs
+            // 
+            this.TBlbsTOkgs.Location = new System.Drawing.Point(150, 115);
+            this.TBlbsTOkgs.Name = "TBlbsTOkgs";
+            this.TBlbsTOkgs.Size = new System.Drawing.Size(100, 20);
+            this.TBlbsTOkgs.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(45, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "pounds (lbs)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(293, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "kilograms (kgs)";
+            // 
+            // weigth_convertor_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(425, 279);
-            this.Name = "weigth_convertorForm";
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TBlbsTOkgs);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "weigth_convertor_Form";
             this.Text = "weigth convertor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox TBlbsTOkgs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- weigth_convertor_Form.cs
+++ weigth_convertor_Form.cs
@@ -12,9 +12,55 @@ namespace IA2_CALCULATOR_srfn
 {
     public partial class weigth_convertor_Form : Form
     {
+        Double numDouble = 0.00;
+        Double numDouble2 = 0.00;
         public weigth_convertor_Form()
         {
             InitializeComponent();
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            if (TBlbsTOkgs.Text == "0")
+            {
+                //no hace nada
+            }
+            else
+            {
+                //TryParse
+                if (double.TryParse(TBlbsTOkgs.Text, out numDouble))
+                {
+                    //MessageBox.Show("string converted to double nicely");
+                    numDouble2 = numDouble * 0.453592;
+                    TBlbsTOkgs.Text = numDouble2.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("shit!");
+                }
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            if (TBlbsTOkgs.Text == "0")
+            {
+                //no hace nada
+            }
+            else
+            {
+                //TryParse
+                if (double.TryParse(TBlbsTOkgs.Text, out numDouble))
+                {
+                    //MessageBox.Show("string converted to double nicely");
+                    numDouble2 = numDouble * 2.20462;
+                    TBlbsTOkgs.Text = numDouble2.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("shit!");
+                }
+            }
         }
     }
 }
